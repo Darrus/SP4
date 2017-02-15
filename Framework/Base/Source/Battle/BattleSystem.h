d1885a08295354d3c7514b6376e1452236c8629a
Slapyousilly <n00b1ass@gmail.com>
2017-02-15 17:09:14 +0800

    Completed Simple Battle System. Modified Stats to a default value. Added entity to BattleState, to able to run Battle System.
    Changed CharacterInfo and InfoBase to a Class

--- Framework/Base/Source/Battle/BattleSystem.h
+++ Framework/Base/Source/Battle/BattleSystem.h
@@ -6,22 +6,31 @@
 #include "..\Character\CharacterInfo.h"
 #include "..\Party\PartySystem.h"
 #include "..\Entity\BattleEntity.h"
+#include "..\Entity\SpriteEntity.h"
 
 class BattleSystem
 {
 private:
     bool anEntityTurn;
     bool iCrit;
+    SpriteEntity* CommandBox;
+
 public:
     BattleSystem();
+    //BattleSystem();
     ~BattleSystem();
 
     virtual void Update();
     virtual void Render();
 
+    BattleEntity* ChooseAtkTarget(BattleEntity* entity);
+
     void EntityTurn(BattleEntity* entity);      ///< Give an Entity the Turn
 
     void Attack(BattleEntity* entity, BattleEntity* targetEntity);  ///< Attack the targetEntity
+    void SpellCast(BattleEntity* entity, BattleEntity* targetEntity);  ///< Attack the targetEntity
+    void Defend(BattleEntity* entity);
+    void PassTurn(BattleEntity* entity);
 
     PartySystem* playerParty;                   ///< Get the player's Party
     std::list<BattleEntity*> BattleList;
