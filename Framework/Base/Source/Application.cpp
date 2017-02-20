59b48cf2b3d1fdcee86a9f4373e7ca879dff0e69
Darrus <darrusgoh@gmail.com>
2017-02-20 22:02:47 +0800

    Pushed in filters. Changed Application Scene Init to mine HAHA! - Goh Zheng Yuan

--- Framework/Base/Source/Application.cpp
+++ Framework/Base/Source/Application.cpp
@@ -140,7 +140,7 @@ void Application::Init()
 	SceneManager::GetInstance()->AddScene("Shop", new ShopScene());
 	
 	//Set the active scene
-	SceneManager::GetInstance()->SetActiveScene("Shop");
+	SceneManager::GetInstance()->SetActiveScene("Overworld");
 
 	//Load Font Data
 	LoadFontData("FontData//pixelFontData.csv");
